14887d6dd0393b4e2d57609b2965206a412a746b
gabrielgggg477 <gabriel_garciagarcia@alumnos-santagema.es>
2025-09-30 20:29:23 +0200

    activity

---
+++ Intels_Tema2_GGG/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Intels_Tema2_GGG"
+include(":app")
+ 
