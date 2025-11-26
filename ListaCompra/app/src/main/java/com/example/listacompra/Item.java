63295ccec8f5c242da84c7e3ddab9fe1c75e8642
gabrielgggg477 <gabriel_garciagarcia@alumnos-santagema.es>
2025-11-26 20:16:09 +0100

    utlimo

--- ListaCompra/app/src/main/java/com/example/listacompra/Item.java
+++ ListaCompra/app/src/main/java/com/example/listacompra/Item.java
@@ -5,18 +5,36 @@ public class Item {
     private int cantidad;
     private int imagenResId;
 
+    // Constructor
     public Item(String nombre, int cantidad, int imagenResId) {
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.imagenResId = imagenResId;
     }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
 
-    public int getCantidad() { return cantidad; }
-    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+    public int getCantidad() {
+        return cantidad;
+    }
 
-    public int getImagenResId() { return imagenResId; }
-    public void setImagenResId(int imagenResId) { this.imagenResId = imagenResId; }
+    public int getImagenResId() {
+        return imagenResId;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setImagenResId(int imagenResId) {
+        this.imagenResId = imagenResId;
+    }
 }
